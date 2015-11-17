6b72b2f3dd32e36c994020f875a319e3cab12702
Scott Matthewman <scott@altmetric.com>
2015-11-17 15:31:24 +0000

    Create a crude Rails generator to start using HouseStyle
    For Rails projects, create a generator that creates `.rubocop.yml`
    config files in the project root and in the RSpec folder. This assumes
    that RSpec is in use.
    
    Usage:
    
    1. Add `gem 'house_style'` to the Gemfile
    2. Run `bundle install`
    3. Run `rails generate house_style:install`
    
    The generator assumes that neither Rubocop config files exists. If
    they do, the user will be prompted to overwrite, ignore or study the
    diff, just as for other generators' file conflicts.

---
+++ lib/generators/house_style/install_generator.rb
@@ -0,0 +1,18 @@
+require 'rails/generators'
+require 'rails/generators/base'
+
+module HouseStyle
+  class InstallGenerator < Rails::Generators::Base
+    def self.source_root
+      @source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+    end
+
+    def create_root_rubocop_yml
+      copy_file '.rubocop.yml', '.rubocop.yml'
+    end
+
+    def create_rspec_rubocop_yml
+      copy_file '.rspec-rubocop.yml', 'spec/.rubocop.yml'
+    end
+  end
+end
